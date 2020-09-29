9b879cef4a28dcef276ddf7c848f1eb3970cf28b
Cristian Hosu <Cristian.Hosu@endava.com>
2020-09-29 16:04:56 +0300

    added missing files

---
+++ src/common-components/CardView.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="card">
+        <div class="title">
+            <slot name="header"> </slot>
+        </div>
+        <div class="close">X</div>
+        <div class="body">
+            <slot></slot>
+        </div>
+        <div class="actions">
+            <slot name="actions" :extras="extras">
+                <button @click="close">Close</button>
+            </slot>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {
+            extras: { shit: false, rainbows: true }
+        };
+    },
+    methods: {
+        close() {}
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+.card {
+    display: grid;
+    grid-template-areas:
+        'title   .       .       close'
+        'body    body    body    body'
+        'actions actions actions actions';
+    width: 400px;
+    height: 600px;
+    padding: 20px 10px;
+    gap: 20px;
+
+    .title {
+        grid-area: title;
+        font-size: 2rem;
+        color: tomato;
+    }
+
+    .close {
+        grid-area: close;
+        font-size: 2rem;
+        color: tomato;
+    }
+
+    .body {
+        grid-area: body;
+    }
+
+    .actions {
+        border-top: 2px solid #bada55;
+        grid-area: actions;
+        > * {
+            all: none;
+            color: fuchsia;
+        }
+    }
+}
+</style>
