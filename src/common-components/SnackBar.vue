9f762c3cc2350eb8c16a4bdd3394ed547712cf4d
Claudiu Gilmeanu <claudiu.gilmeanu@endava.com>
2020-09-30 14:28:49 +0300

    Add vuex

---
+++ src/common-components/SnackBar.vue
@@ -0,0 +1,38 @@
+<template>
+    <v-snackbar
+        bottom
+        right
+        v-model="snackbar.show"
+        :color="snackbar.color"
+        :timeout="snackbar.timeout"
+    >
+        {{ snackbar.message }}
+        <v-btn text @click="snackbar.show = false">Close</v-btn>
+    </v-snackbar>
+</template>
+
+<script>
+import { mapActions, mapState } from 'vuex';
+
+export default {
+    data: () => ({}),
+    computed: {
+        ...mapState('snackbarModule', ['snackbar'])
+    },
+    mounted() {
+        //let error = { message: 'Bau bau!' };
+        //this.showError(error);
+        this.showSuccess({ message: 'Hello! Welcome to ower pizza store' });
+    },
+    methods: {
+        ...mapActions('snackbarModule', [
+            'showSuccess',
+            'showError',
+            'showWarning',
+            'hide'
+        ])
+    }
+};
+</script>
+
+<style></style>
---
+++ src/store/snackbar/index.js
@@ -0,0 +1,76 @@
+const state = {
+    snackbar: {
+        show: false,
+        message: `It's alive`,
+        timeout: 2000,
+        color: 'success'
+    }
+};
+
+const actions = {
+    showSuccess({ commit }, payload) {
+        commit('setSuccessSnackBar', payload);
+    },
+    addToOrder({ commit }, payload) {
+        commit('setSuccessSnackBar', payload);
+    },
+    showError({ commit }, error) {
+        commit('showError', error.message);
+    },
+    showWarning({ commit }, message) {
+        commit('showWarning', message);
+    },
+    hide({ commit }) {
+        commit('hide');
+    }
+};
+
+const mutations = {
+    setSuccessSnackBar(state, payload) {
+        state.snackbar = {
+            ...state.snackbar,
+            message: payload.message,
+            color: 'success',
+            timeout: 8000,
+            show: true
+        };
+    },
+    showWarning(state, message) {
+        state.snackbar = {
+            ...state.snackbar,
+            message,
+            color: 'warning',
+            timeout: 3000,
+            show: true
+        };
+    },
+    showError(state, message) {
+        state.snackbar = {
+            ...state.snackbar,
+            message,
+            color: 'warning',
+            timeout: 0,
+            show: true
+        };
+    },
+
+    hide(state) {
+        state.snackBar = {
+            ...state.snackbar,
+            message: '',
+            color: 'success',
+            timeout: 2000,
+            show: false
+        };
+    }
+};
+
+const getters = {};
+
+export const snackBar = {
+    state,
+    actions,
+    mutations,
+    getters,
+    namespaced: true
+};
